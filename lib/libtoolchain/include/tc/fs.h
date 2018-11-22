0011151d5fd590dbfdaf4ae2cdd924da195bb591
jakcron <jakcron.dev@gmail.com>
2018-11-22 22:21:36 +0800

    Add fs header file.

---
+++ lib/libtoolchain/include/tc/fs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Objects
+#include <tc/fs/Path.h>
+#include <tc/fs/DirectoryInfo.h>
+
+// Interfaces
+#include <tc/fs/IFile.h>
+#include <tc/fs/IFileSystem.h>
+
+// Local FileSystem
+#include <tc/fs/LocalFile.h>
+#include <tc/fs/LocalFileSystem.h>
