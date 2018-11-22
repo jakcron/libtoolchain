78c41ea0cfc4f08e201de77337a0e8aeb6b63445
jakcron <jakcron.dev@gmail.com>
2018-11-22 22:04:22 +0800

    Add exception class

---
+++ lib/libtoolchain/include/tc/Exception.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <exception>
+#include <string>
+
+namespace tc
+{
+	class Exception : public std::exception
+	{
+	public:
+		Exception() noexcept;
+		Exception(const std::string& what) noexcept;
+		Exception(const std::string& module, const std::string& what) noexcept;
+
+		const char* what() const noexcept;
+		const char* module() const noexcept;
+		const char* error() const noexcept;
+	private:
+		std::string what_;
+		std::string module_;
+		std::string error_;
+	};
+}
