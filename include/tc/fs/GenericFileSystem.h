ed1bcd55456b17884e6dc739f49ebcfa7564c280
jakcron <jakcron.dev@gmail.com>
2019-06-25 19:57:38 +0800

    Copy/Move operators/constructors for Generic types are now equivalent to implicit versions, so remove.

--- include/tc/fs/GenericFileSystem.h
+++ include/tc/fs/GenericFileSystem.h
@@ -37,37 +37,6 @@ public:
 		 */
 	GenericFileSystem(tc::fs::IFileSystem&& other);
 
-		/**
-		 * @brief Copy Constructor
-		 * @param[in] other GenericFileSystem object to copy
-		 */
-	GenericFileSystem(const tc::fs::GenericFileSystem& other);
-
-		/**
-		 * @brief Move Constructor
-		 * @param[in] other GenericFileSystem object to move
-		 */
-	GenericFileSystem(tc::fs::GenericFileSystem&& other);
-
-		/// Destructor
-	~GenericFileSystem();
-
-		/**
-		 * @brief Copy Assignment Operator
-		 * @param[in] other GenericFileSystem object to copy
-		 * 
-		 * @return this object
-		 */
-	GenericFileSystem& operator=(const tc::fs::GenericFileSystem& other);
-
-		/**
-		 * @brief Move Assignment Operator
-		 * @param[in] other GenericFileSystem object to move
-		 * 
-		 * @return this object
-		 */
-	GenericFileSystem& operator=(tc::fs::GenericFileSystem&& other);
-
 	virtual tc::fs::IFileSystem* copyInstance() const;
 	virtual tc::fs::IFileSystem* moveInstance();
 
