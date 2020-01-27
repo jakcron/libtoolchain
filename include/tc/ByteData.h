d6c1da1209a4726f0d3071be636aca09b1f4b68a
Jack <jakcron.dev@gmail.com>
2020-01-27 18:04:41 +0800

    Add ByteData

---
+++ include/tc/ByteData.h
@@ -0,0 +1,50 @@
+	/**
+	 * @file ByteData.h
+	 * @brief Declaration of tc::ByteData
+	 * @author Jack (jakcron)
+	 * @version 0.1
+	 * @date 2020/01/27
+	 **/
+#pragma once
+#include <tc/types.h>
+
+#include <tc/OutOfMemoryException.h>
+
+namespace tc {
+
+	/**
+	 * @class ByteData
+	 * @brief A container of linear memory, used to hold raw data.
+	 **/
+class ByteData
+{
+public:
+		/**
+		 * @brief Create linear memory block
+		 * 
+		 * @param[in] size Size in bytes of the memory block
+		 * 
+		 * @throw tc::OutOfMemoryException Insuffient memory available.
+		 **/
+	ByteData(size_t size);
+		
+		/**
+		 * @brief Get buffer pointer
+		 **/
+	const byte_t* buffer() const { return mPtr.get(); }
+
+		/**
+		 * @brief Get buffer size
+		 **/
+	size_t size() const { return mSize; }
+private:
+	static const std::string kClassName;
+
+	// deleted default constructor
+	ByteData();
+
+	size_t mSize;
+	std::shared_ptr<byte_t> mPtr;
+}; 
+
+} // namespace tc
