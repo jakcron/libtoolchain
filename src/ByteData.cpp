27c4cae4def8453f380d869d1947233f628e76fb
Jack <jakcron.dev@gmail.com>
2020-01-27 18:01:47 +0800

    Add ByteData

---
+++ src/ByteData.cpp
@@ -0,0 +1,12 @@
+#include <tc/ByteData.h>
+
+const std::string tc::ByteData::kClassName = "tc::ByteData";
+
+tc::ByteData::ByteData(size_t size)
+{
+	mPtr = std::shared_ptr<byte_t>(new byte_t[size]);
+	if (mPtr.get() == nullptr)
+	{
+		throw tc::OutOfMemoryException(kClassName);
+	}
+}
