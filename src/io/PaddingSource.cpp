278ab29af04ecebc1ecbb2032d93d58b1ff8a0e1
Jack <jakcron.dev@gmail.com>
2020-02-07 21:16:06 +0800

    Add PaddingSource

---
+++ src/io/PaddingSource.cpp
@@ -0,0 +1,28 @@
+#include <tc/io/PaddingSource.h>
+#include <tc/io/SourceUtil.h>
+
+tc::io::PaddingSource::PaddingSource() :
+	mSourceLength(0),
+	mPaddingByte(0)
+{
+}
+
+tc::io::PaddingSource::PaddingSource(byte_t padding_byte, int64_t size) :
+	mSourceLength(size),
+	mPaddingByte(padding_byte)
+{
+}
+
+int64_t tc::io::PaddingSource::length()
+{
+	return mSourceLength;
+}
+
+tc::ByteData tc::io::PaddingSource::pullData(int64_t offset, size_t count)
+{
+	tc::ByteData data(SourceUtil::getPullableCount(mSourceLength, offset, count));
+
+	memset(data.buffer(), mPaddingByte, data.size());
+	
+	return data;
+}
