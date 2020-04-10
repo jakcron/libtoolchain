1469bb19f98791f117a3f67eca95922d27d81bd0
Jack <jakcron.dev@gmail.com>
2020-04-10 20:12:27 +0800

    Include missed change.

--- include/tc/io/SubStorage.h
+++ include/tc/io/SubStorage.h
@@ -121,7 +121,7 @@ private:
 	
 	std::shared_ptr<tc::io::IStorage> mBaseStorage;
 	tc::io::Path mBaseStoragePath;
-	std::shared_ptr<tc::io::Path> mSubStoragePath;
+	tc::io::Path mSubStoragePath;
 
 	void subPathToRealPath(const tc::io::Path& substorage_path, tc::io::Path& real_path);
 	void realPathToSubPath(const tc::io::Path& real_path, tc::io::Path& substorage_path);
