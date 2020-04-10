95c28c04356fcae59fd8e2e72167f1f06135a274
Jack <jakcron.dev@gmail.com>
2020-04-10 23:51:43 +0800

    Misc documentation fix.

--- include/tc/NotImplementedException.h
+++ include/tc/NotImplementedException.h
@@ -12,7 +12,7 @@ namespace tc {
 
 	/**
 	 * @class NotImplementedException
-	 * @brief The exception that is thrown when a requested method or operation is not implemented
+	 * @brief The exception that is thrown when a requested method or operation is not implemented.
 	 **/
 class NotImplementedException : public tc::Exception
 {
