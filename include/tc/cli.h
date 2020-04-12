0a1e218dc155a4e46dde66d68ae7a0f2a21ff84c
Jack <jakcron.dev@gmail.com>
2020-04-12 13:51:59 +0800

    Add cli.h

---
+++ include/tc/cli.h
@@ -0,0 +1,12 @@
+	/**
+	 * @file		cli.h
+	 * @brief       Declaration of the command-line interface (CLI) library
+	 */
+#pragma once
+#include <tc/types.h>
+#include <tc/Exception.h>
+
+	/**
+	 * @namespace   tc::cli
+	 * @brief       Namespace of the command-line interface (CLI) library
+	 */
