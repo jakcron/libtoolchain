ecf0ee21c89a3e2d13e7ad76cc8e636a2689444e
Jack <jakcron.dev@gmail.com>
2020-04-12 13:16:59 +0800

    Add crypto header.

---
+++ include/tc/crypto.h
@@ -0,0 +1,12 @@
+	/**
+	 * @file		crypto.h
+	 * @brief       Declaration of the cryptography library
+	 */
+#pragma once
+#include <tc/types.h>
+#include <tc/Exception.h>
+
+	/**
+	 * @namespace   tc::crypto
+	 * @brief       Namespace of the cryptography library
+	 */
