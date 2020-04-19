5f807e92c01f89f24cca0fa23205144a0c341fd0
Jack <jakcron.dev@gmail.com>
2020-04-19 22:40:27 +0800

    [ISigner] Revise interface.

--- include/tc/crypto/ISigner.h
+++ include/tc/crypto/ISigner.h
@@ -3,7 +3,7 @@
 	 * @brief Declaration of tc::crypto::ISigner
 	 * @author Jack (jakcron)
 	 * @version 0.1
-	 * @date 2020/04/15
+	 * @date 2020/04/19
 	 **/
 #pragma once
 
@@ -17,12 +17,12 @@ class ISigner
 public:
 	virtual ~ISigner() = default;
 
-	virtual size_t GetKeySize() const = 0;
-	virtual size_t GetBlockSize() const = 0;
-	virtual size_t GetSignatureSize() const = 0;
+	virtual size_t block_size() const = 0;
+	virtual size_t key_size() const = 0;
+	virtual size_t signature_size() const = 0;
 
-	virtual void SignBlock(byte_t* signature, size_t signature_size, const byte_t* src, size_t src_size) = 0;
-	virtual bool VerifyBlock(const byte_t* signature, size_t signature_size, const byte_t* src, size_t src_size) = 0;
+	virtual void sign(byte_t* signature, const byte_t* src, size_t src_size) = 0;
+	virtual bool verify(const byte_t* signature, const byte_t* src, size_t src_size) = 0;
 };
 
 }}
