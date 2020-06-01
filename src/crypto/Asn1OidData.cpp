ff615ea8f7d4e006c4e7dd5692249fb7aa2547f8
Jack <jakcron.dev@gmail.com>
2020-06-01 18:10:57 +0800

    Add Md5Generator & GenerateMd5Hash

--- src/crypto/Asn1OidData.cpp
+++ src/crypto/Asn1OidData.cpp
@@ -1,3 +1,5 @@
-#include <tc/crypto/Sha1Generator.h>
+#include <tc/crypto/Md5Generator.h>
+const std::array<byte_t, tc::crypto::Md5Generator::kAsn1OidDataSize> tc::crypto::Md5Generator::kAsn1OidData = {0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x02, 0x05};
 
+#include <tc/crypto/Sha1Generator.h>
 const std::array<byte_t, tc::crypto::Sha1Generator::kAsn1OidDataSize> tc::crypto::Sha1Generator::kAsn1OidData = {0x2b, 0x0e, 0x03, 0x02, 0x1a};
