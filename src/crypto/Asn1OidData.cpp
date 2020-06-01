06425926c014ca0910c5ba732c6794f8c8b82b75
Jack <jakcron.dev@gmail.com>
2020-06-01 18:15:09 +0800

    Add Sha256Generator & GenerateSha256Hash

--- src/crypto/Asn1OidData.cpp
+++ src/crypto/Asn1OidData.cpp
@@ -3,3 +3,6 @@ const std::array<byte_t, tc::crypto::Md5Generator::kAsn1OidDataSize> tc::crypto:
 
 #include <tc/crypto/Sha1Generator.h>
 const std::array<byte_t, tc::crypto::Sha1Generator::kAsn1OidDataSize> tc::crypto::Sha1Generator::kAsn1OidData = {0x2b, 0x0e, 0x03, 0x02, 0x1a};
+
+#include <tc/crypto/Sha256Generator.h>
+const std::array<byte_t, tc::crypto::Sha256Generator::kAsn1OidDataSize> tc::crypto::Sha256Generator::kAsn1OidData = {0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01};
