f62ad0bee8163d3329ed38178991a2cba53f41ef
jakcron <jakcron.dev@gmail.com>
2020-09-13 18:28:58 +0800

    Fix copy paste mistake.

--- src/crypto/RsaPkcs1Sha512Signer.cpp
+++ src/crypto/RsaPkcs1Sha512Signer.cpp
@@ -1,43 +1,43 @@
-#include <tc/crypto/RsaPkcs1Sha1Signer.h>
+#include <tc/crypto/RsaPkcs1Sha512Signer.h>
 
-bool tc::crypto::SignRsa1024Pkcs1Sha1(byte_t* signature, const byte_t* message_digest, const RsaKey& key)
+bool tc::crypto::SignRsa1024Pkcs1Sha512(byte_t* signature, const byte_t* message_digest, const RsaKey& key)
 {
-	tc::crypto::Rsa1024Pkcs1Sha1Signer impl;
+	tc::crypto::Rsa1024Pkcs1Sha512Signer impl;
 	impl.initialize(key);
 	return impl.sign(signature, message_digest);
 }
 
-bool tc::crypto::VerifyRsa1024Pkcs1Sha1(const byte_t* signature, const byte_t* message_digest, const RsaKey& key)
+bool tc::crypto::VerifyRsa1024Pkcs1Sha512(const byte_t* signature, const byte_t* message_digest, const RsaKey& key)
 {
-	tc::crypto::Rsa1024Pkcs1Sha1Signer impl;
+	tc::crypto::Rsa1024Pkcs1Sha512Signer impl;
 	impl.initialize(key);
 	return impl.verify(signature, message_digest);
 }
 
-bool tc::crypto::SignRsa2048Pkcs1Sha1(byte_t* signature, const byte_t* message_digest, const RsaKey& key)
+bool tc::crypto::SignRsa2048Pkcs1Sha512(byte_t* signature, const byte_t* message_digest, const RsaKey& key)
 {
-	tc::crypto::Rsa2048Pkcs1Sha1Signer impl;
+	tc::crypto::Rsa2048Pkcs1Sha512Signer impl;
 	impl.initialize(key);
 	return impl.sign(signature, message_digest);
 }
 
-bool tc::crypto::VerifyRsa2048Pkcs1Sha1(const byte_t* signature, const byte_t* message_digest, const RsaKey& key)
+bool tc::crypto::VerifyRsa2048Pkcs1Sha512(const byte_t* signature, const byte_t* message_digest, const RsaKey& key)
 {
-	tc::crypto::Rsa2048Pkcs1Sha1Signer impl;
+	tc::crypto::Rsa2048Pkcs1Sha512Signer impl;
 	impl.initialize(key);
 	return impl.verify(signature, message_digest);
 }
 
-bool tc::crypto::SignRsa4096Pkcs1Sha1(byte_t* signature, const byte_t* message_digest, const RsaKey& key)
+bool tc::crypto::SignRsa4096Pkcs1Sha512(byte_t* signature, const byte_t* message_digest, const RsaKey& key)
 {
-	tc::crypto::Rsa4096Pkcs1Sha1Signer impl;
+	tc::crypto::Rsa4096Pkcs1Sha512Signer impl;
 	impl.initialize(key);
 	return impl.sign(signature, message_digest);
 }
 
-bool tc::crypto::VerifyRsa4096Pkcs1Sha1(const byte_t* signature, const byte_t* message_digest, const RsaKey& key)
+bool tc::crypto::VerifyRsa4096Pkcs1Sha512(const byte_t* signature, const byte_t* message_digest, const RsaKey& key)
 {
-	tc::crypto::Rsa4096Pkcs1Sha1Signer impl;
+	tc::crypto::Rsa4096Pkcs1Sha512Signer impl;
 	impl.initialize(key);
 	return impl.verify(signature, message_digest);
 }
