6647b0f1f9d8ef64fb299182224ef3f80139dd72
jakcron <jakcron.dev@gmail.com>
2020-09-19 16:13:02 +0800

    Add RsaPssSigner for SHA2-256,512 and RSA-1024,2048,4096.

---
+++ include/tc/crypto/RsaPssSigner.h
@@ -0,0 +1,197 @@
+	/**
+	 * @file RsaPssSigner.h
+	 * @brief Declaration of tc::crypto::RsaPssSigner
+	 * @author Jack (jakcron)
+	 * @version 0.2
+	 * @date 2020/09/19
+	 **/
+#pragma once
+#include <tc/types.h>
+#include <tc/crypto/detail/RsaImpl.h>
+#include <tc/crypto/detail/PrbgImpl.h>
+#include <tc/crypto/detail/RsaPssPadding.h>
+#include <tc/crypto/RsaKey.h>
+
+
+namespace tc { namespace crypto {
+
+	/**
+	 * @class RsaPssSigner
+	 * @brief Class for calculating an RSA-PSS signature.
+	 * 
+	 * @tparam KeySize Size in bytes of RSA key size.
+	 * @tparam HashFunction The class that implements the hash function used with RSA-PSS.
+	 * 
+	 * @details
+	 * This class is a template class that takes a hash function implementation class as template parameter.
+	 * See @ref Rsa2048PssSha256Signer or similar for supplied realizations of this template class.
+	 * 
+	 * The implementation of <var>HashFunction</var> must satisfies the following conditions.
+	 * See @ref Sha256Generator or similar class, for more information including parameters to each function.
+	 * 
+	 * -# Has a <tt>kBlockSize</tt> constant that defines the size of the block to process.
+	 * -# Has a <tt>kHashSize</tt> constant that defines the output size of the hash value.
+	 * -# Has an <tt>initialize</tt> method that begins processing.
+	 * -# Has an <tt>update</tt> method that updates the hash value on input.
+	 * -# Has a <tt>getHash</tt> method that gets the final hash value.
+	 * 
+	 * This class has two states:
+	 * - None : Not ready
+	 * - Initialized : Ready to process input data
+	 * 
+	 * General usage of this class is as follows:
+	 * - Initialize RSA Signer state with @ref initialize().
+	 * - Sign/Verify message digest with @ref sign() / @ref verify().
+	 */
+template <size_t KeySize, class HashFunction>
+class RsaPssSigner
+{
+public:
+	static const size_t kSignatureSize = KeySize; /**< RSA-PSS signature size */
+
+		/**
+		 * @brief Default constructor.
+		 * 
+		 * @post
+		 * - State is None. @ref initialize() must be called before use.
+		 */
+	RsaPssSigner() :
+		mState(State::None),
+		mRsaImpl(),
+		mPrbgImpl(),
+		mPadImpl()
+	{}
+
+		/**
+		 * @brief Initializes the signature calculation.
+		 * 
+		 * @param[in] key Pointer to key data.
+		 * @param[in] key_size Size in bytes of key data.
+		 * 
+		 * @post
+		 * - Instance is now in a Initialized state
+		 * 
+		 * @details
+		 * Resets the RSA calculation state with an RSA key.
+		 */
+	void initialize(const RsaKey& key)
+	{
+		mRsaImpl.initialize(KeySize << 3, key.n.data(), key.n.size(), nullptr, 0, nullptr, 0, key.d.data(), key.d.size(), key.e.data(), key.e.size());
+		
+		mState = State::Initialized;
+	}
+	
+		/**
+		 * @brief Calculate RSA-PSS signature.
+		 * 
+		 * @param[out] signature Pointer to the buffer storing the signature.
+		 * @param[in]  message_digest Pointer to message digest.
+		 * @return true if signature calculation was successful.
+		 * 
+		 * @pre
+		 * - Size of the signature buffer must >= <tt>Rsa1024PssSha256Signer::kSignatureSize</tt>.
+		 * 
+		 * @post
+		 * - The signature is written to <tt><var>signature</var></tt>.
+		 * 
+		 * @details
+		 * This function calculates a signature for a message digest.
+		 * This generates the salt internally. To manually specify the salt, please use the alternative @ref sign() method.
+		 */
+	bool sign(byte_t* signature, const byte_t* message_digest)
+	{
+		if (mState != State::Initialized) { return false; }
+
+		std::array<byte_t, HashFunction::kHashSize> salt;
+		mPrbgImpl.getBytes(salt.data(), salt.size());
+		// usable_salt_size: salt.size(), but if the blocksize isn't big enough, then its = KeySize - (HashFunction::kHashSize + 2)
+		// this may produce an illegal salt size (salt_size < HashFunction::kHashSize - 2), but this will be caught by BuildPad()
+		size_t usable_salt_size = std::min<size_t>(salt.size(), KeySize - (HashFunction::kHashSize + 2));
+
+		return sign(signature, message_digest, salt.data(), usable_salt_size);
+	}
+
+		/**
+		 * @brief Calculate RSA-PSS signature.
+		 * 
+		 * @param[out] signature Pointer to the buffer storing the signature.
+		 * @param[in]  message_digest Pointer to message digest.
+		 * @param[in]  salt Pointer to salt.
+		 * @param[in]  salt_size Size of @p salt.
+		 * @return true if signature calculation was successful.
+		 * 
+		 * @pre
+		 * - Size of the signature buffer must >= <tt>Rsa1024PssSha256Signer::kSignatureSize</tt>.
+		 * - The data in @p salt should be random, otherwise the signature strength is reduced.
+		 * - Where KeySize >= (HashCalculator::kHashSize * 2 + 2) @p salt_size = HashCalculator::kHashSize. Otherwise @p salt_size = KeySize - (HashCalculator::kHashSize + 2). However the minimum legal salt size is (HashCalculator::kHashSize - 2), if the salt_size falls below this consider a larger KeySize as this operation will not complete successfully.
+		 * 
+		 * @post
+		 * - The signature is written to <tt><var>signature</var></tt>.
+		 * 
+		 * @details
+		 * This function calculates a signature for a message digest.
+		 */
+	bool sign(byte_t* signature, const byte_t* message_digest, const byte_t* salt, size_t salt_size)
+	{
+		if (mState != State::Initialized) { return false; }
+
+		std::array<byte_t, kSignatureSize> block;
+		memset(block.data(), 0, block.size());
+
+		if (mPadImpl.BuildPad(block.data(), block.size(), message_digest, HashFunction::kHashSize, salt, salt_size, (KeySize << 3) - 1) != detail::RsaPssPadding<HashFunction>::Result::kSuccess)
+		{
+			return false;
+		} 
+
+		try
+		{
+			mRsaImpl.privateTransform(signature, block.data());
+		} 
+		catch (...)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+		/**
+		 * @brief Verify RSA-PSS signature.
+		 * 
+		 * @param[in] signature Pointer to signature.
+		 * @param[in] message_digest Pointer to message digest.
+		 * @return true if the signature is valid, otherwise false.
+		 * 
+		 * @details
+		 * This function verifies a signature for a message digest.
+		 */
+	bool verify(const byte_t* signature, const byte_t* message_digest)
+	{
+		if (mState != State::Initialized) { return false; }
+
+		std::array<byte_t, kSignatureSize> block;
+		memcpy(block.data(), signature, block.size());
+
+		try {
+			mRsaImpl.publicTransform(block.data(), signature);
+		} catch (...) {
+			return false;
+		}
+				
+		return (mPadImpl.CheckPad(message_digest, HashFunction::kHashSize, block.data(), block.size(), (KeySize << 3) - 1) == detail::RsaPssPadding<HashFunction>::Result::kSuccess);
+	}
+
+private:
+	enum class State
+	{
+		None,
+		Initialized
+	};
+
+	State mState;
+	detail::RsaImpl mRsaImpl;
+	detail::PrbgImpl mPrbgImpl;
+	detail::RsaPssPadding<HashFunction> mPadImpl;
+};
+
+}} // namespace tc::crypto
