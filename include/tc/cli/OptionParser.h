6e22df5eb4036159a67ebc751a7b4ffd641d70a1
jakcron <jakcron.dev@gmail.com>
2021-08-01 20:05:51 +0800

    Update documentation.

--- include/tc/cli/OptionParser.h
+++ include/tc/cli/OptionParser.h
@@ -17,6 +17,104 @@ namespace tc { namespace cli {
 	/**
 	 * @class OptionParser
 	 * @brief Class for parsing command-line options
+	 * 
+	 * This class processes command-line options according to user defined implementations of @ref tc::cli::OptionParser::IOptionHandler that are registered with this class.
+	 * 
+	 * The format of command-line arguments varies by system and convention. This class supports the following styles of command-line options:
+	 * * "--opt": Option name prefixed by "--" with no parameters,
+	 * * "--opt=var": Option name prefixed by "--" with only one parameter delimetered by "=",
+	 * * "--opt var1 var2": Option name prefixed by "--" with one or more parameters delimited by white space,
+	 * * "-opt": Option name prefixed by "-" with no parameters,
+	 * * "-opt=var": Option name prefixed by "-" with one parameter delimetered by "=",
+	 * * "-opt var1 var2": Option name prefixed by "-" with one or more parameters delimited by white space,
+	 * 
+	 * Command-line options take the form of an option name prefixed with one or two dash characters (e.g. "-i" or "--infile-path") followed by zero or more option values.
+	 * Some conventions will allow multiple aliases for a given option (e.g. "-r", "-R", "--recursive"). 
+	 * 
+	 * Option values can be considered as parameters to the option. Examples include:
+	 * 1) Toggling a flag withing program logic: e.g. enabling read-only mode for a forensics tool may used "--read-only" or "-r" as options with no parameters, since the name implies the state.
+	 * 2) Passing a variable into the program: e.g. setting the file path for an input file, may use "--infile path/to/file" or "-i path/to/file", an option with one parameter.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * For example, say we have some state struct like this:
+	 * @code
+	 * struct UserOpt
+	 * {
+	 *    std::string sku_code;
+	 * };
+	 * @endcode 
+	 * 
+	 * And the command-line is intended be formatted as follows:
+	 * @code
+	 * someprogram -sku <your SKU code here>
+	 * @endcode
+	 * 
+	 * Then a possible IOptionHandler could be implemented as follows:
+	 * @code
+	 * class SkuOptionHandler : public tc::cli::OptionParser::IOptionHandler
+	 * {
+	 * public:
+	 * 	SkuOptionHandler(UserOpt& user_opt) : 
+	 * 		mUserOpt(user_opt),
+	 * 		mOptStrings({"-sku"})
+	 * 	{}
+	 * 
+	 * 	const std::vector<std::string>& getOptionStrings() const
+	 * 	{
+	 * 		return mOptStrings;
+	 * 	}
+	 * 
+	 * 	void processOption(const std::string& option, const std::vector<std::string>& params)
+	 * 	{
+	 * 		// validate number of paramaters (in this case you we only want 1 parameter)
+	 * 		if (params.size() != 1)
+	 * 		{
+	 * 			throw tc::ArgumentOutOfRangeException("Option \"" + option + "\" requires a parameter.");
+	 * 		}
+	 * 
+	 * 		mUserOpt.sku_code = params[0];
+	 * 	}
+	 * private:
+	 * 	UserOpt& mUserOpt;
+	 * 	std::vector<std::string> mOptStrings;
+	 * };
+	 * @endcode
+	 * 
+	 * Then register SkuOptionHandler with the OptionParser using OptionParser::registerOptionHandler():
+	 * @code
+	 * 
+	 * UserOpt user_opt;
+	 * 
+	 * tc::cli::OptionParser opt_parser;
+	 * 
+	 * opt_parser.registerOptionHandler(std::shared_ptr<SkuOptionHandler>(new SkuOptionHandler(user_opt)))
+	 * 
+	 * @endcode
+	 * 
+	 * Then process the command-line arguments with OptionParser::processOptions():
+	 * @code
+	 * // In this example processOptions
+	 * int umain(const std::vector<std::string>& args, const std::vector<std::string>& env)
+	 * {
+	 * 	UserOpt user_opt;
+	 * 	tc::cli::OptionParser opt_parser;
+	 * 
+	 * 	// register the option handler for "-sku"
+	 * 	opt_parser.registerOptionHandler(std::shared_ptr<SkuOptionHandler>(new SkuOptionHandler(user_opt)))
+	 * 
+	 * 	// since args will include at args[0], the program executable path, use the overload of processOptions that selects a sub vector of args.
+	 * 	opt_parser.processOptions(args, 1, args.size()-1);
+	 * 
+	 * 	// user_opt.sku_type will now be populated if it was set via command-line with "-sku"
+	 * 
+	 * 	std::cout << "SKUCODE: \"" << user_opt.sku_code << "\"" << std::endl;
+	 * 
+	 * 	// finish program
+	 * 	return 0;
+	 * }
+	 * @endcode
 	 */
 class OptionParser
 {
