0899d5eb4345ef9026c00c5d1fad685e443651af
jakcron <jakcron.dev@gmail.com>
2021-08-02 13:55:27 +0800

    Finish first draft of OptionParser documentation.

--- include/tc/cli/OptionParser.h
+++ include/tc/cli/OptionParser.h
@@ -15,7 +15,7 @@ namespace tc { namespace cli {
 
 	/**
 	 * @class OptionParser
-	 * @brief Class for parsing command-line options
+	 * @brief Class for parsing command-line options.
 	 * 
 	 * This class processes command-line options according to user defined implementations of @ref tc::cli::OptionParser::IOptionHandler that are registered with this class.
 	 * 
@@ -129,22 +129,63 @@ namespace tc { namespace cli {
 class OptionParser
 {
 public:
+
+		/**
+		 * @class IOptionHandler
+		 * @brief Interface for handling command-line options and any parameters, to be used with @ref OptionParser.
+		 * 
+		 * See @ref OptionParser for an example on how to implement this class.
+		 */
 	class IOptionHandler
 	{
 	public:
 		virtual ~IOptionHandler() = default;
 
+			/**
+			 * @brief Returns a vector of aliases for the option this will handle.
+			 */
 		virtual const std::vector<std::string>& getOptionStrings() const = 0;
 
+			/**
+			 * @brief Processes command-line option and any parameters.
+			 * 
+			 * @param[in] option This is full option name that was used.
+			 * @param[in] params This is a vector of parameters that were supplied with the option name, size may be 0 or more.
+			 */ 
 		virtual void processOption(const std::string& option, const std::vector<std::string>& params) = 0;
 	};
 
 	OptionParser();
 
+
+		/**
+		 * @brief Register an IOptionHandler to handle an option.
+		 * 
+		 * @param[in] handler Shared pointer to the IOptionHandler.
+		 */ 
 	void registerOptionHandler(const std::shared_ptr<IOptionHandler>& handler);
+
+		/**
+		 * @brief Register an IOptionHandler to handle all unrecognised options. Only use this extra processing of unrecognised options is required beyond a generic exception.
+		 * 
+		 * @param[in] handler Shared pointer to the IOptionHandler.
+		 */ 
 	void registerUnrecognisedOptionHandler(const std::shared_ptr<IOptionHandler>& handler);
 
+		/**
+		 * @brief Process a vector of command-line args in accordance with the registered option handlers.
+		 * 
+		 * @param[in] args Command-line args to process.
+		 */
 	void processOptions(const std::vector<std::string>& args);
+
+		/**
+		 * @brief Process a vector of command-line args in accordance with the registered option handlers.
+		 * 
+		 * @param[in] args Command-line args to process.
+		 * @param[in] pos Position in args vector to begin with.
+		 * @param[in] num Number of elements in the args vector to process.
+		 */
 	void processOptions(const std::vector<std::string>& args, size_t pos, size_t num);
 
 private:
