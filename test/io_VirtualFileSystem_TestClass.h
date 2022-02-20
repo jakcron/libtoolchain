5017b9273e6c27566d645c85b56b3cf9e6561f0b
jakcron <jakcron.dev@gmail.com>
2022-02-20 11:01:55 +0800

    Add tests for VirtualFileSystem

--- test/io_VirtualFileSystem_TestClass.h
+++ test/io_VirtualFileSystem_TestClass.h
@@ -1,13 +1,24 @@
 #pragma once
 #include "ITestClass.h"
 
-#include <tc/io.h>
-#include <tc/ArgumentOutOfRangeException.h>
-#include <tc/NotImplementedException.h>
+#include <tc/io/VirtualFileSystem.h>
+
 
 class io_VirtualFileSystem_TestClass : public ITestClass
 {
 public:
 	void runAllTests();
 private:
+	void test_CreateUninitializedFs_DefaultConstructor();
+	void test_BadFsSnapshot_CreateConstructor();
+	void test_CreateFs_CreateConstructor();
+	void test_ThrowsOnBadPermissions_OpenFile();
+	void test_ThrowsOnBadFileEntry_OpenFile();
+	void test_ThrowsOnBadFileEntry_GetDirectoryListing();
+	void test_ThrowsOnBadFileEntry_SetWorkingDirectory();
+	void test_WorksForAllValidPaths_OpenFile();
+	void test_WorksForAllValidPaths_GetDirectoryListing();
+	void test_WorksForAllValidPaths_SetWorkingDirectory();
+	void test_WorksForAllValidPaths_GetWorkingDirectory();
+	void test_DisposeWillChangeStateToUninitialized();
 };
