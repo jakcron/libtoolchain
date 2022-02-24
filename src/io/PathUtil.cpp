dce1224dae707c606c025f148126c31480a5b51d
jakcron <jakcron.dev@gmail.com>
2022-02-24 19:23:22 +0800

    Update PathUtil impl to use Path to_string() and to_u16string().

--- src/io/PathUtil.cpp
+++ src/io/PathUtil.cpp
@@ -3,30 +3,10 @@
 
 void tc::io::PathUtil::pathToWindowsUTF16(const tc::io::Path& path, std::u16string& out)
 {
-	std::u16string utf16_element;
-
-	out.clear();
-	for (tc::io::Path::const_iterator itr = path.begin(); itr != path.end(); itr++)
-	{
-		tc::string::TranscodeUtil::UTF8ToUTF16(*itr, utf16_element);
-
-		out += utf16_element;
-		if (itr != --path.end())
-			out += std::u16string(u"\\");
-		if (utf16_element == std::u16string(u"") && itr == path.begin())
-			out += std::u16string(u"\\");
-	}
+	out = path.to_u16string(tc::io::Path::Format::Win32);
 }
 
 void tc::io::PathUtil::pathToUnixUTF8(const tc::io::Path& path, std::string& out)
 {
-	out.clear();
-	for (tc::io::Path::const_iterator itr = path.begin(); itr != path.end(); itr++)
-	{
-		out += *itr;
-		if (itr != --path.end())
-			out += "/";
-		else if (*itr == "" && itr == path.begin())
-			out += "/";
-	}
+	out = path.to_string(tc::io::Path::Format::POSIX);
 }
