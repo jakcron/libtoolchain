19bffe7b3760b2fa930406fddd5db548bb273b2c
jakcron <jakcron.dev@gmail.com>
2022-03-10 17:57:07 +0800

    Alternative way to swap endianness.

--- include/tc/bn/endian_types.h
+++ include/tc/bn/endian_types.h
@@ -15,6 +15,11 @@ static inline uint16_t __local_bswap16(uint16_t x) {
 	return ((x << 8) & 0xff00) | ((x >> 8) & 0x00ff);
 }
 
+static inline void __local_bswap16(void* x) {
+	uint16_t tmp = *((uint16_t*)x);
+	*((uint16_t*)x) = ((tmp << 8) & 0xff00) | ((tmp >> 8) & 0x00ff);
+}
+
 static inline uint32_t __local_bswap32(uint32_t x) {
 	return	((x << 24) & 0xff000000 ) |
 			((x <<  8) & 0x00ff0000 ) |
@@ -22,10 +27,30 @@ static inline uint32_t __local_bswap32(uint32_t x) {
 			((x >> 24) & 0x000000ff );
 }
 
+static inline void __local_bswap32(void* x) {
+	uint32_t tmp = *((uint32_t*)x);
+	*((uint32_t*)x) =	((tmp << 24) & 0xff000000 ) |
+						((tmp <<  8) & 0x00ff0000 ) |
+						((tmp >>  8) & 0x0000ff00 ) |
+						((tmp >> 24) & 0x000000ff );
+}
+
 static inline uint64_t __local_bswap64(uint64_t x)
 {
 	return (uint64_t)__local_bswap32(x>>32) |
 	      ((uint64_t)__local_bswap32(x&0xFFFFFFFF) << 32);
+}
+
+static inline void __local_bswap64(void* x) {
+	uint64_t tmp = *((uint64_t*)x);
+	*((uint64_t*)x) =	((tmp << 54) & 0xff00000000000000ULL ) |
+						((tmp << 40) & 0x00ff000000000000ULL ) |
+						((tmp << 24) & 0x0000ff0000000000ULL ) |
+						((tmp <<  8) & 0x000000ff00000000ULL ) |
+						((tmp >>  8) & 0x00000000ff000000ULL ) |
+						((tmp >> 24) & 0x0000000000ff0000ULL ) |
+						((tmp >> 40) & 0x000000000000ff00ULL ) |
+						((tmp >> 54) & 0x00000000000000ffULL );
 }
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
@@ -35,6 +60,14 @@ static inline uint16_t __be_uint16(uint16_t a) { return __local_bswap16(a); }
 static inline uint64_t __le_uint64(uint64_t a) { return a; }
 static inline uint32_t __le_uint32(uint32_t a) { return a; }
 static inline uint16_t __le_uint16(uint16_t a) { return a; }
+
+static inline void __be_swap64(void* a) { __local_bswap64(a); }
+static inline void __be_swap32(void* a) { __local_bswap32(a); }
+static inline void __be_swap16(void* a) { __local_bswap16(a); }
+static inline void __le_swap64(void* a) { return; }
+static inline void __le_swap32(void* a) { return; }
+static inline void __le_swap16(void* a) { return; }
+
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 static inline uint64_t __be_uint64(uint64_t a) { return a; }
 static inline uint32_t __be_uint32(uint32_t a) { return a; }
@@ -42,6 +75,13 @@ static inline uint16_t __be_uint16(uint16_t a) { return a; }
 static inline uint64_t __le_uint64(uint64_t a) { return __local_bswap64(a); }
 static inline uint32_t __le_uint32(uint32_t a) { return __local_bswap32(a); }
 static inline uint16_t __le_uint16(uint16_t a) { return __local_bswap16(a); }
+
+static inline void __be_swap64(void* a) { return; }
+static inline void __be_swap32(void* a) { return; }
+static inline void __be_swap16(void* a) { return; }
+static inline void __le_swap64(void* a) { __local_bswap64(a); }
+static inline void __le_swap32(void* a) { __local_bswap32(a); }
+static inline void __le_swap16(void* a) { __local_bswap16(a); }
 #endif
 
 }}} // namespace tc::bn::detail
@@ -64,9 +104,9 @@ public:
 	le16& operator=(const T& var) { wrap(var); return *this; }
 
 		/// Unwrap value
-	inline T unwrap() const { return (T)detail::__le_uint16((uint16_t)mVar); }
+	inline T unwrap() const { T tmp = mVar; detail::__le_swap16(&tmp); return tmp; }
 		/// Wrap value
-	inline void wrap(const T& var) { mVar = (T)detail::__le_uint16((uint16_t)var); }
+	inline void wrap(const T& var) { mVar = var; detail::__le_swap16(&mVar); }
 private:
 	T mVar;
 };
@@ -87,9 +127,9 @@ public:
 	be16& operator=(const T& var) { wrap(var); return *this; }
 
 		/// Unwrap value
-	inline T unwrap() const { return (T)detail::__be_uint16((uint16_t)mVar); }
+	inline T unwrap() const { T tmp = mVar; detail::__be_swap16(&tmp); return tmp; }
 		/// Wrap value
-	inline void wrap(const T& var) { mVar = (T)detail::__be_uint16((uint16_t)var); }
+	inline void wrap(const T& var) { mVar = var; detail::__be_swap16(&mVar); }
 private:
 	T mVar;
 };
@@ -110,9 +150,9 @@ public:
 	le32& operator=(const T& var) { wrap(var); return *this; }
 
 		/// Unwrap value
-	inline T unwrap() const { return (T)detail::__le_uint32((uint32_t)mVar); }
+	inline T unwrap() const { T tmp = mVar; detail::__le_swap32(&tmp); return tmp; }
 		/// Wrap value
-	inline void wrap(const T& var) { mVar = (T)detail::__le_uint32((uint32_t)var); }
+	inline void wrap(const T& var) { mVar = var; detail::__le_swap32(&mVar); }
 private:
 	T mVar;
 };
@@ -133,9 +173,9 @@ public:
 	be32& operator=(const T& var) { wrap(var); return *this; }
 
 		/// Unwrap value
-	inline T unwrap() const { return (T)detail::__be_uint32((uint32_t)mVar); }
+	inline T unwrap() const { T tmp = mVar; detail::__be_swap32(&tmp); return tmp; }
 		/// Wrap value
-	inline void wrap(const T& var) { mVar = (T)detail::__be_uint32((uint32_t)var); }
+	inline void wrap(const T& var) { mVar = var; detail::__be_swap32(&mVar); }
 private:
 	T mVar;
 };
@@ -156,9 +196,9 @@ public:
 	le64& operator=(const T& var) { wrap(var); return *this; }
 
 		/// Unwrap value
-	inline T unwrap() const { return (T)detail::__le_uint64((uint64_t)mVar); }
+	inline T unwrap() const { T tmp = mVar; detail::__le_swap64(&tmp); return tmp; }
 		/// Wrap value
-	inline void wrap(const T& var) { mVar = (T)detail::__le_uint64((uint64_t)var); }
+	inline void wrap(const T& var) { mVar = var; detail::__le_swap64(&mVar); }
 private:
 	T mVar;
 };
@@ -179,9 +219,9 @@ public:
 	be64& operator=(const T& var) { wrap(var); return *this; }
 
 		/// Unwrap value
-	inline T unwrap() const { return (T)detail::__be_uint64((uint64_t)mVar); }
+	inline T unwrap() const { T tmp = mVar; detail::__be_swap64(&tmp); return tmp; }
 		/// Wrap value
-	inline void wrap(const T& var) { mVar = (T)detail::__be_uint64((uint64_t)var); }
+	inline void wrap(const T& var) { mVar = var; detail::__be_swap64(&mVar); }
 private:
 	T mVar;
 };
