ab5c230ce10d5f1e80f02a9f66375ea6f8201db4
Jack <jack@azulon.net>
2022-08-07 14:39:35 +0800

    Convert crypto_Sha256Generator_TestClass

--- test/main.cpp
+++ test/main.cpp
@@ -29,6 +29,7 @@
 #include "io_StreamSink_TestClass.h"
 #include "crypto_Md5Generator_TestClass.h"
 #include "crypto_Sha1Generator_TestClass.h"
+#include "crypto_Sha256Generator_TestClass.h"
 
 
 /*
@@ -36,7 +37,6 @@
 
 
 
-#include "crypto_Sha256Generator_TestClass.h"
 #include "crypto_Sha512Generator_TestClass.h"
 #include "crypto_HmacMd5Generator_TestClass.h"
 #include "crypto_HmacSha1Generator_TestClass.h"
@@ -250,12 +250,12 @@ int main(int argc, char** argv)
 	// namespace tc::crypto
 	runTest<crypto_Md5Generator_TestClass>(global_test_results, include_test_regex, exclude_test_regex);
 	runTest<crypto_Sha1Generator_TestClass>(global_test_results, include_test_regex, exclude_test_regex);
+	runTest<crypto_Sha256Generator_TestClass>(global_test_results, include_test_regex, exclude_test_regex);
 
 	/*
 	
 	
 	
-	runTest<crypto_Sha256Generator_TestClass>(global_test_results, include_test_regex, exclude_test_regex);
 	runTest<crypto_Sha512Generator_TestClass>(global_test_results, include_test_regex, exclude_test_regex);
 	runTest<crypto_HmacMd5Generator_TestClass>(global_test_results, include_test_regex, exclude_test_regex);
 	runTest<crypto_HmacSha1Generator_TestClass>(global_test_results, include_test_regex, exclude_test_regex);
