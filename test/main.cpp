b3997fc85d0fc87974b5f319fa292df3a5df1d17
Jack <jack@azulon.net>
2022-08-14 13:27:26 +0800

    Convert crypto_Pbkdf2Sha512KeyDeriver_TestClass

--- test/main.cpp
+++ test/main.cpp
@@ -39,10 +39,10 @@
 #include "crypto_Pbkdf1Sha1KeyDeriver_TestClass.h"
 #include "crypto_Pbkdf2Sha1KeyDeriver_TestClass.h"
 #include "crypto_Pbkdf2Sha256KeyDeriver_TestClass.h"
+#include "crypto_Pbkdf2Sha512KeyDeriver_TestClass.h"
 
 
 /*
-#include "crypto_Pbkdf2Sha512KeyDeriver_TestClass.h"
 #include "crypto_PseudoRandomByteGenerator_TestClass.h"
 #include "crypto_Aes128Encryptor_TestClass.h"
 #include "crypto_Aes192Encryptor_TestClass.h"
@@ -261,13 +261,14 @@ int main(int argc, char** argv)
 	runTest<crypto_Pbkdf1Sha1KeyDeriver_TestClass>(global_test_results, include_test_regex, exclude_test_regex, include_slow_tests);
 	runTest<crypto_Pbkdf2Sha1KeyDeriver_TestClass>(global_test_results, include_test_regex, exclude_test_regex, include_slow_tests);
 	runTest<crypto_Pbkdf2Sha256KeyDeriver_TestClass>(global_test_results, include_test_regex, exclude_test_regex, include_slow_tests);
+	runTest<crypto_Pbkdf2Sha512KeyDeriver_TestClass>(global_test_results, include_test_regex, exclude_test_regex, include_slow_tests);
 
 
 	/*
 	
 	if (include_slow_tests)
 	{
-		runTest<crypto_Pbkdf2Sha512KeyDeriver_TestClass>(global_test_results, include_test_regex, exclude_test_regex, include_slow_tests);
+		
 	}
 	runTest<crypto_PseudoRandomByteGenerator_TestClass>(global_test_results, include_test_regex, exclude_test_regex, include_slow_tests);
 	runTest<crypto_Aes128Encryptor_TestClass>(global_test_results, include_test_regex, exclude_test_regex, include_slow_tests);
