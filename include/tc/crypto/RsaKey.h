da6fc8cf09f4ccc053784f8868d6b9903665276d
Jack <jack@azulon.net>
2025-07-18 10:11:21 +0800

    Allow specifying custom public exponent for RSA keys

--- include/tc/crypto/RsaKey.h
+++ include/tc/crypto/RsaKey.h
@@ -2,8 +2,8 @@
 	 * @file RsaKey.h
 	 * @brief Declarations for structures to store RSA keys.
 	 * @author Jack (jakcron)
-	 * @version 0.1
-	 * @date 2020/08/27
+	 * @version 0.2
+	 * @date 2025/06/01
 	 **/
 #pragma once
 #include <tc/types.h>
@@ -18,28 +18,41 @@ namespace tc { namespace crypto {
 struct RsaKey
 {
 	tc::ByteData n; /**< Modulus */
-	tc::ByteData d; /**< Private exponent */
 	tc::ByteData e; /**< Public exponent */
+	tc::ByteData d; /**< Private exponent */
 };
 
 	/**
 	 * @struct RsaPublicKey
-	 * @brief This extends RsaKey, exposing a constructor to create a RSA public key from a modulus.
+	 * @brief This extends RsaKey, exposing a constructor to create a RSA public key from a modulus (and optionally a public exponent).
 	 */
 struct RsaPublicKey : public RsaKey
 {
 		/**
-		 * @brief This constructs a @ref RsaKey from a modulus.
+		 * @brief This constructs a @ref RsaKey from a modulus and the default public exponent (0x10001).
 		 * 
 		 * @param[in] modulus Buffer containing big-endian modulus.
 		 * @param[in] modulus_size Size in bytes of modulus.
 		 * 
 		 * @pre @p modulus != nullptr
 		 * @pre @p modulus_size != 0
-		 * 
-		 * @details Supplying a public exponent is not required, as this is the same for all RSA keys and is initialized internally by this constructor.
 		 */
 	RsaPublicKey(const byte_t* modulus, size_t modulus_size);
+
+		/**
+		 * @brief This constructs a @ref RsaKey from a modulus, and a public exponent.
+		 * 
+		 * @param[in] modulus Buffer containing big-endian modulus.
+		 * @param[in] modulus_size Size in bytes of modulus.
+		 * @param[in] public_exponent Buffer containing big-endian public exponent.
+		 * @param[in] public_exponent_size Size in bytes of public exponent.
+		 * 
+		 * @pre @p modulus != nullptr
+		 * @pre @p modulus_size != 0
+		 * @pre @p public_exponent != nullptr
+		 * @pre @p public_exponent_size != 0
+		 */
+	RsaPublicKey(const byte_t* modulus, size_t modulus_size, const byte_t* public_exponent, size_t public_exponent_size);
 };
 
 	/**
@@ -49,7 +62,7 @@ struct RsaPublicKey : public RsaKey
 struct RsaPrivateKey : public RsaKey
 {
 		/**
-		 * @brief This constructs a @ref RsaKey from a modulus and private exponent.
+		 * @brief This constructs a @ref RsaKey from a modulus, private exponent, and the default public exponent (0x10001).
 		 * 
 		 * @param[in] modulus Buffer containing big-endian modulus.
 		 * @param[in] modulus_size Size in bytes of modulus.
@@ -62,6 +75,25 @@ struct RsaPrivateKey : public RsaKey
 		 * @pre @p private_exponent_size != 0
 		 */
 	RsaPrivateKey(const byte_t* modulus, size_t modulus_size, const byte_t* private_exponent, size_t private_exponent_size);
+
+		/**
+		 * @brief This constructs a @ref RsaKey from a modulus, private exponent, and the default public exponent (0x10001).
+		 * 
+		 * @param[in] modulus Buffer containing big-endian modulus.
+		 * @param[in] modulus_size Size in bytes of modulus.
+		 * @param[in] public_exponent Buffer containing big-endian public exponent.
+		 * @param[in] public_exponent_size Size in bytes of public exponent.
+		 * @param[in] private_exponent Buffer containing big-endian private exponent.
+		 * @param[in] private_exponent_size Size in bytes of private exponent.
+		 * 
+		 * @pre @p modulus != nullptr
+		 * @pre @p modulus_size != 0
+		 * @pre @p private_exponent != nullptr
+		 * @pre @p private_exponent_size != 0
+		 * @pre @p public_exponent != nullptr
+		 * @pre @p public_exponent_size != 0
+		 */
+	RsaPrivateKey(const byte_t* modulus, size_t modulus_size, const byte_t* public_exponent, size_t public_exponent_size, const byte_t* private_exponent, size_t private_exponent_size);
 
 
 		/**
--- src/crypto/RsaKey.cpp
+++ src/crypto/RsaKey.cpp
@@ -1,29 +1,38 @@
 #include <tc/crypto/RsaKey.h>
 
-tc::crypto::RsaPublicKey::RsaPublicKey(const byte_t* modulus, size_t modulus_size)
-{
-	static const byte_t kPublicExponent[3] = { 0x01, 0x00, 0x01 };
+static const byte_t kPublicExponent[3] = { 0x01, 0x00, 0x01 };
 
-	if (modulus != nullptr && modulus_size != 0)
+tc::crypto::RsaPublicKey::RsaPublicKey(const byte_t* modulus, size_t modulus_size) :
+	RsaPublicKey(modulus, modulus_size, kPublicExponent, sizeof(kPublicExponent))
+{
+}
+
+tc::crypto::RsaPublicKey::RsaPublicKey(const byte_t* modulus, size_t modulus_size, const byte_t* public_exponent, size_t public_exponent_size)
+{
+	if (modulus != nullptr && modulus_size != 0 && public_exponent != nullptr && public_exponent_size != 0)
 	{
 		this->n = tc::ByteData(modulus, modulus_size);
-		this->e = tc::ByteData(kPublicExponent, sizeof(kPublicExponent));
+		this->e = tc::ByteData(public_exponent, public_exponent_size);
+		this->d = tc::ByteData();
 	}
 }
 
-tc::crypto::RsaPrivateKey::RsaPrivateKey(const byte_t* modulus, size_t modulus_size, const byte_t* private_exponent, size_t private_exponent_size)
+tc::crypto::RsaPrivateKey::RsaPrivateKey(const byte_t* modulus, size_t modulus_size, const byte_t* private_exponent, size_t private_exponent_size) :
+	RsaPrivateKey(modulus, modulus_size, kPublicExponent, sizeof(kPublicExponent), private_exponent, private_exponent_size)
 {
-	static const byte_t kPublicExponent[3] = { 0x01, 0x00, 0x01 };
+}
 
-	if (modulus != nullptr && modulus_size != 0 && private_exponent != nullptr && private_exponent_size != 0)
+tc::crypto::RsaPrivateKey::RsaPrivateKey(const byte_t* modulus, size_t modulus_size, const byte_t* public_exponent, size_t public_exponent_size, const byte_t* private_exponent, size_t private_exponent_size)
+{
+	if (modulus != nullptr && modulus_size != 0 && public_exponent != nullptr && public_exponent_size != 0 && private_exponent != nullptr && private_exponent_size != 0)
 	{
 		this->n = tc::ByteData(modulus, modulus_size);
+		this->e = tc::ByteData(public_exponent, public_exponent_size);
 		this->d = tc::ByteData(private_exponent, private_exponent_size);
-		this->e = tc::ByteData(kPublicExponent, sizeof(kPublicExponent));
 	}
 }
 
 tc::crypto::RsaKey tc::crypto::RsaPrivateKey::getPublicKey()
 {
-	return RsaPublicKey(this->n.data(), this->n.size());
+	return RsaPublicKey(this->n.data(), this->n.size(), this->e.data(), this->e.size());
 }
