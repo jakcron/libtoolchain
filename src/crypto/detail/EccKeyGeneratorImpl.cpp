56585951f3561675f419e769cec2be257f6ea5f6
jakcron <jakcron.dev@gmail.com>
2025-08-03 18:36:03 +1000

    EccKeyGeneratorImpl - include mbedtls error in exception message.

--- src/crypto/detail/EccKeyGeneratorImpl.cpp
+++ src/crypto/detail/EccKeyGeneratorImpl.cpp
@@ -3,6 +3,7 @@
 #include <mbedtls/rsa.h>
 #include <mbedtls/ctr_drbg.h>
 #include <mbedtls/entropy.h>
+#include <fmt/core.h>
 
 const std::string tc::crypto::detail::EccKeyGeneratorImpl::kClassName = "tc::crypto::detail::EccKeyGeneratorImpl";
 
@@ -18,7 +19,7 @@ tc::crypto::detail::EccKeyGeneratorImpl::EccKeyGeneratorImpl() :
 	mbedtls_entropy_init( &(mImplCtx->entropy) );
 	mbedtls_ctr_drbg_init( &(mImplCtx->ctr_drbg) );
 
-	int ret = mbedtls_ctr_drbg_seed(&(mImplCtx->ctr_drbg), mbedtls_entropy_func, &(mImplCtx->entropy), (const unsigned char *)kClassName.c_str(), kClassName.size());
+	int ret = mbedtls_ctr_drbg_seed( &(mImplCtx->ctr_drbg), mbedtls_entropy_func, &(mImplCtx->entropy), (const unsigned char *)kClassName.c_str(), kClassName.size() );
 	switch (ret)
 	{
 		case (0):
@@ -90,7 +91,7 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generateKey(EccCurveType ec_type, 
 	mbedtls_ecp_group_id group_id = convertToMbedtlsEcpGroupId(ec_type);
 	if (group_id == MBEDTLS_ECP_DP_NONE)
 	{
-		throw tc::ArgumentOutOfRangeException(kClassName, "ec_type was not valid.");
+		throw tc::ArgumentOutOfRangeException(kClassName, fmt::format("ec_type ({}) was not valid.", (uint32_t)ec_type));
 	}
 
 	mbedtls_ecp_keypair key;
@@ -99,40 +100,43 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generateKey(EccCurveType ec_type, 
 	int ret = 1;
 	
 	// generate key
-	ret = mbedtls_ecp_gen_key(group_id, &key, mbedtls_ctr_drbg_random, &(mImplCtx->ctr_drbg));
+	ret = mbedtls_ecp_gen_key( group_id, &key, mbedtls_ctr_drbg_random, &(mImplCtx->ctr_drbg) );
 	switch (ret)
 	{
 		case (0):
 			break;
 		default:
-			throw tc::crypto::CryptoException(kClassName, "mbedtls_ecp_gen_key() An unexpected error occurred.");
+			throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_ecp_gen_key() An unexpected error occurred. {:x})", ret));
+			
 	}
 	
+	// determine curve integer length for this key
+	size_t p_len = mbedtls_mpi_size( &key.grp.P );
+
 	// export private component
 	if (d != nullptr)
 	{
-		size_t d_len = mbedtls_mpi_size( &key.d );
-		if (d_size < d_len)
+		if (d_size < p_len)
 		{
 			throw tc::ArgumentNullException(kClassName, "d was not null, but d_size was insufficent to store private exponent");
 		}
 
-		ret = mbedtls_mpi_write_binary( &key.d, d, std::min<size_t>(d_size, d_len) );
+		ret = mbedtls_mpi_write_binary( &key.d, d, std::min<size_t>(d_size, p_len) );
 		switch (ret)
 		{
 			case (0):
 				break;
 			//case (MBEDTLS_ERR_MPI_BUFFER_TOO_SMALL):
 			default:
-				throw tc::crypto::CryptoException(kClassName, "mbedtls_mpi_write_binary() An unexpected error occurred.");
+				throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_mpi_write_binary() An unexpected error occurred. {:x})", ret));
 		}
 	}
 	
 	// export public component
 	if (q != nullptr)
 	{
-		size_t p_len = mbedtls_mpi_size( &key.grp.P );
-
+		// when writing the public component, it seems to be standard to reserve the full integer point for X,Y of Q, regardless of how many bits are actually used
+		// even if Q.Y is empty (length 0)
 		// if key is < min size
 		if (q_size < (p_len * 2))
 		{
@@ -146,7 +150,7 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generateKey(EccCurveType ec_type, 
 				break;
 			//case (MBEDTLS_ERR_MPI_BUFFER_TOO_SMALL):
 			default:
-				throw tc::crypto::CryptoException(kClassName, "mbedtls_mpi_write_binary() An unexpected error occurred.");
+				throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_mpi_write_binary() An unexpected error occurred. {:x})", ret));
 		}
 
 		ret = mbedtls_mpi_write_binary( &key.Q.Y, q + p_len, p_len );
@@ -156,7 +160,7 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generateKey(EccCurveType ec_type, 
 				break;
 			//case (MBEDTLS_ERR_MPI_BUFFER_TOO_SMALL):
 			default:
-				throw tc::crypto::CryptoException(kClassName, "mbedtls_mpi_write_binary() An unexpected error occurred.");
+				throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_mpi_write_binary() An unexpected error occurred. {:x})", ret));
 		}
 	}
 	
@@ -181,7 +185,7 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generatePublicKey(EccCurveType ec_
 		case (0):
 			break;
 		default:
-			throw tc::crypto::CryptoException(kClassName, "mbedtls_ecp_group_load() An unexpected error occurred.");
+			throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_ecp_group_load() An unexpected error occurred. {:x})", ret));
 	}
 
 	// import private component (d)
@@ -192,7 +196,7 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generatePublicKey(EccCurveType ec_
 			break;
 		//case (MBEDTLS_ERR_MPI_BUFFER_TOO_SMALL):
 		default:
-			throw tc::crypto::CryptoException(kClassName, "mbedtls_mpi_read_binary() An unexpected error occurred.");
+			throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_mpi_read_binary() An unexpected error occurred.", ret));
 	}
 
 	// generate public component (Q) from private exponent (d) and base point (G)
@@ -201,8 +205,10 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generatePublicKey(EccCurveType ec_
 	{
 		case (0):
 			break;
+		case (MBEDTLS_ERR_ECP_INVALID_KEY):
+			throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_ecp_mul() Invalid public or private key. {:x})", ret));
 		default:
-			throw tc::crypto::CryptoException(kClassName, "mbedtls_ecp_mul() An unexpected error occurred.");
+			throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_ecp_mul() An unexpected error occurred. {:x})", ret));
 	}
 	
 	// export public component
@@ -223,7 +229,7 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generatePublicKey(EccCurveType ec_
 				break;
 			//case (MBEDTLS_ERR_MPI_BUFFER_TOO_SMALL):
 			default:
-				throw tc::crypto::CryptoException(kClassName, "mbedtls_mpi_write_binary() An unexpected error occurred.");
+				throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_mpi_write_binary() An unexpected error occurred. {:x})", ret));
 		}
 
 		ret = mbedtls_mpi_write_binary( &key.Q.Y, q + p_len, p_len );
@@ -233,7 +239,7 @@ void tc::crypto::detail::EccKeyGeneratorImpl::generatePublicKey(EccCurveType ec_
 				break;
 			//case (MBEDTLS_ERR_MPI_BUFFER_TOO_SMALL):
 			default:
-				throw tc::crypto::CryptoException(kClassName, "mbedtls_mpi_write_binary() An unexpected error occurred.");
+				throw tc::crypto::CryptoException(kClassName, fmt::format("mbedtls_mpi_write_binary() An unexpected error occurred. {:x})", ret));
 		}
 	}
 	
