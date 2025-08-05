e778dfd2f2111f8870b92902577a6e8fbae48186
jakcron <jakcron.dev@gmail.com>
2025-08-05 15:00:38 +1000

    Removed windows header from Environment.cpp

--- src/os/Environment.cpp
+++ src/os/Environment.cpp
@@ -1,12 +1,6 @@
 #include <tc/os/Environment.h>
 #include <tc/io/LocalFileSystem.h>
 
-#ifdef _WIN32
-#include <fileapi.h>
-#else
-
-#endif
-
 bool tc::os::getEnvVar(const std::string& name, std::string& value)
 {
 	bool did_find_variable = false;
